9d8d43e8c6d0be1a0f80c1639ee0430cb83edca1
Catissocute <98471871+Catissocute@users.noreply.github.com>
2022-12-24 15:11:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,67 @@
+function playa() {
+    var x = document.getElementById("a");
+      x.play();
+    }
+function playb() {
+var x = document.getElementById("b");
+  x.play();
+}
+function playc() {
+    var x = document.getElementById("c");
+      x.play();
+}
+function playd() {
+var x = document.getElementById("d");
+  x.play();
+}
+function playe() {
+    var x = document.getElementById("e");
+      x.play();
+}
+function playf() {
+var x = document.getElementById("f");
+  x.play();
+}
+function playg() {
+    var x = document.getElementById("g");
+      x.play();
+}
+function playh() {
+    var x = document.getElementById("h");
+    x.play();
+}
+function playi() {
+    var x = document.getElementById("i");
+      x.play();
+}
+function playj() {
+    var x = document.getElementById("j");
+      x.play();
+    }
+function playk() {
+var x = document.getElementById("k");
+  x.play();
+}
+function playl() {
+var x = document.getElementById("l");
+  x.play();
+}
+function playm() {
+    var x = document.getElementById("m");
+      x.play();
+    }
+function playn() {
+var x = document.getElementById("n");
+  x.play();
+}
+function playo() {
+    var x = document.getElementById("o");
+      x.play();
+    }
+function playp() {
+var x = document.getElementById("p");
+  x.play();
+}
+        
+    
+                   
